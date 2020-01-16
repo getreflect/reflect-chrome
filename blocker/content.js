c8acc684f7d9d6bdf275aefaff0084ecb36efd15
jackyzha0 <j.zhao2k19@gmail.com>
2020-01-15 18:13:15 -0800

    adding css injection

--- blocker/content.js
+++ blocker/content.js
@@ -1,6 +1,5 @@
 (function () {
 	chrome.storage.sync.get('isEnabled', function(data) {
-
 		// check to see if reflect is enabled
 		if (data.isEnabled) {
 
@@ -8,7 +7,20 @@
 			chrome.storage.sync.get('blockedSites', function(data) {
 				data.blockedSites.forEach(function(site) {
 					if (window.location.href.includes(site)) {
-						$(":root").load(chrome.runtime.getURL("res/prompt.html"));
+						oldDat = $(":root").html()
+						$.get(chrome.runtime.getURL("res/pages/prompt.html"), function(page) {
+					        document.open();
+						    document.write(page);
+						    document.close();
+
+    						var cssPath = chrome.runtime.getURL('res/common.css');
+							$("head").append(
+								$('<link rel="stylesheet" type="text/css" />').attr('href', cssPath)
+							);
+						})
+						// save url to cache
+
+						// load css and images
 					}
 				});
 			});
