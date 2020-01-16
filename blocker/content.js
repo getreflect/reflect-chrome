eed050402c2f22efa1c347b02d625ae0cbb059e8
jackyzha0 <j.zhao2k19@gmail.com>
2020-01-15 19:51:04 -0800

    add last few elements to block screen

--- blocker/content.js
+++ blocker/content.js
@@ -20,6 +20,8 @@
 
 							$("#top-left-blob").attr("src", chrome.runtime.getURL('res/blob-big-2.svg'))
 							$("#bottom-right-blob").attr("src", chrome.runtime.getURL('res/blob-big-1.svg'))
+							$("#small-blob1").attr("src", chrome.runtime.getURL('res/blob-med.svg'))
+							$("#small-blob2").attr("src", chrome.runtime.getURL('res/blob-small.svg'))
 						})
 						// save url to cache
 
