4975b8630a1eb61065fb5c59b3eea8a0c9db0736
jackyzha0 <j.zhao2k19@gmail.com>
2020-03-05 14:14:18 -0800

    wip tf commit

---
+++ src/nn.ts
@@ -0,0 +1,36 @@
+import * as tf from '@tensorflow/tfjs';
+
+class IntentClassifier {
+
+	model: tf.GraphModel;
+	readonly SEQ_MAX_LEN = 75;
+	readonly myReadOnlyProperty = 1;
+
+	constructor() {
+		this.loadModel();
+	}
+
+
+  async loadModel() {
+	console.log('Loading model...');
+
+	// start a performance timer
+	const startTime = performance.now();
+
+	// attempt to load model
+	try {
+	  this.model = await tf.loadGraphModel('res/models/acc82.61/model.js');
+
+	  // Warms up the model by causing intermediate tensor values
+	  // to be built and pushed to GPU.
+	  tf.tidy(() => {
+		this.model.predict(tf.zeros([1, IMAGE_SIZE, IMAGE_SIZE, 3]));
+	  });
+
+	  const totalTime = Math.floor(performance.now() - startTime);
+	  console.log(`Model loaded and initialized in ${totalTime} ms...`);
+	} catch {
+	  console.error(`Unable to load model.`);
+	}
+  }
+}
