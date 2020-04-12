ff8e7a99df8d4e0f072ae5241f637774188a62be
jackyzha0 <j.zhao2k19@gmail.com>
2020-04-11 17:21:06 -0700

    fix bad path in manifest.json

--- dist/src/options.js
+++ dist/src/options.js
@@ -1,22 +1,28 @@
 const ENTER_KEY_CODE = 13;
 // On page load, draw table and add button listener
 document.addEventListener('DOMContentLoaded', function renderFilterListTable() {
-    drawFilterListTable(setDeconsteButtonsListeners);
+    drawFilterListTable();
     setAddButtonListener();
 });
-function setDeconsteButtonsListeners() {
+function updateButtonListeners() {
+    // get all buttons
     const buttons = document.getElementsByTagName("button");
     for (const button of buttons) {
         button.addEventListener("click", () => {
             var _a;
+            // get button ID
             const id = parseInt(button.id[0]);
+            // get url
             const url = (_a = document.getElementById(button.id[0] + "site")) === null || _a === void 0 ? void 0 : _a.innerHTML;
+            // get blockedSites
             chrome.storage.sync.get('blockedSites', (storage) => {
                 const blockedSites = storage.blockedSites;
+                // remove by ID
                 blockedSites.splice(id, 1);
+                // sync with chrome storage
                 chrome.storage.sync.set({ 'blockedSites': blockedSites }, () => {
                     console.log(`removed ${url} from blocked list`);
-                    drawFilterListTable(setDeconsteButtonsListeners);
+                    drawFilterListTable();
                 });
             });
         });
@@ -24,7 +30,7 @@ function setDeconsteButtonsListeners() {
     ;
 }
 ;
-function drawFilterListTable(callback) {
+function drawFilterListTable() {
     chrome.storage.sync.get('blockedSites', (storage) => {
         const blockedSites = storage.blockedSites;
         const tableDiv = document.getElementById('filterList');
@@ -40,37 +46,43 @@ function drawFilterListTable(callback) {
         if (filterList != null) {
             filterList.innerHTML = table;
         }
-        if (callback === undefined) {
-            // create empty callback
-            callback = () => { };
-        }
-        callback();
+        updateButtonListeners();
     });
 }
 ;
+// sets event listeners for add new url operations
 function setAddButtonListener() {
-    var _a, _b;
-    (_a = document.getElementById('urlInput')) === null || _a === void 0 ? void 0 : _a.addEventListener("keypress", (event) => {
+    const urlInputElement = document.getElementById('urlInput');
+    // add key listener to submit new url on <ENTER> pressed
+    urlInputElement.addEventListener("keypress", (event) => {
         if (event.keyCode == ENTER_KEY_CODE) {
             addUrlToFilterList();
         }
     });
+    // add click listener to add URL button
     const addButton = document.getElementById('add');
-    (_b = addButton) === null || _b === void 0 ? void 0 : _b.addEventListener('click', () => {
+    addButton.addEventListener('click', () => {
         addUrlToFilterList();
     });
 }
 ;
 function addUrlToFilterList() {
+    // get urlInput
     const urlInput = document.getElementById('urlInput');
+    // see if value is non-empty
     if (urlInput.value != "") {
         chrome.storage.sync.get('blockedSites', (storage) => {
+            // get current blocked sites
             const blockedSites = storage.blockedSites;
+            // add to blocked sites
             blockedSites.push(urlInput.value);
+            // sync changes with chrome storage
             chrome.storage.sync.set({ 'blockedSites': blockedSites }, () => {
                 console.log(`added ${urlInput} from blocked list`);
+                // clear input
                 urlInput.value = "";
-                drawFilterListTable(setDeconsteButtonsListeners);
+                // redraw filterList
+                drawFilterListTable();
             });
         });
     }
