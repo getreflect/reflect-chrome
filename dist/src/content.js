9de0dbaab025733cc409147aa0db4bebd2603db6
jackyzha0 <j.zhao2k19@gmail.com>
2020-07-26 00:34:19 -0700

    add built files

--- dist/src/content.js
+++ dist/src/content.js
@@ -1,5 +1,5 @@
-const REFLECT_INFO = "#576ca8";
-const REFLECT_ERR = "#ff4a47";
+const REFLECT_INFO = '#576ca8';
+const REFLECT_ERR = '#ff4a47';
 chrome.storage.sync.get(null, (storage) => {
     // check to see if reflect is enabled
     if (storage.isEnabled) {
@@ -15,37 +15,37 @@ chrome.storage.sync.get(null, (storage) => {
 });
 function displayStatus(message, duration = 3000, colour = REFLECT_INFO) {
     // set content
-    $("#statusContent").css("color", colour);
-    $("#statusContent").text(message);
+    $('#statusContent').css('color', colour);
+    $('#statusContent').text(message);
     // show, wait, then hide
-    $("#statusContent").show().delay(duration).fadeOut();
+    $('#statusContent').show().delay(duration).fadeOut();
 }
 function getStrippedUrl() {
     // match url
     const activeURL = window.location.href.match(/^[\w]+:\/{2}([\w\.:-]+)/);
     if (activeURL != null) {
-        const strippedURL = activeURL[1].replace("www.", "");
+        const strippedURL = activeURL[1].replace('www.', '');
         return strippedURL;
     }
     // no url?
-    return "";
+    return '';
 }
 function iterWhitelist() {
     // iterate whitelisted sites
     chrome.storage.sync.get(null, (storage) => {
         const strippedURL = getStrippedUrl();
         // activeURL exists
-        if (strippedURL != "") {
+        if (strippedURL != '') {
             console.log(strippedURL);
             // if url in whitelist
             const m = storage.whitelistedSites;
             if (m.hasOwnProperty(strippedURL)) {
-                console.log("whitelisted");
+                console.log('whitelisted');
                 // check if expired
                 const parsedDate = new Date(m[strippedURL]);
                 const currentDate = new Date();
                 if (currentDate >= parsedDate) {
-                    console.log("expired");
+                    console.log('expired');
                     loadBlockPage(strippedURL);
                 }
                 else {
@@ -55,7 +55,7 @@ function iterWhitelist() {
                 }
             }
             else {
-                console.log("blocked");
+                console.log('blocked');
                 loadBlockPage(strippedURL);
             }
         }
@@ -64,26 +64,26 @@ function iterWhitelist() {
 }
 function loadBlockPage(strippedURL) {
     // get prompt page content
-    $.get(chrome.runtime.getURL("res/pages/prompt.html"), (page) => {
+    $.get(chrome.runtime.getURL('res/pages/prompt.html'), (page) => {
         // stop current page and replace with our blocker page
         window.stop();
         $('html').html(page);
         addFormListener(strippedURL);
         // inject show options page
-        $("#linkToOptions").attr("href", chrome.runtime.getURL('res/pages/options.html'));
+        $('#linkToOptions').attr('href', chrome.runtime.getURL('res/pages/options.html'));
         // load css
         const cssPath = chrome.runtime.getURL('res/common.css');
-        $("head").append($('<link rel="stylesheet" type="text/css" />').attr('href', cssPath));
+        $('head').append($('<link rel="stylesheet" type="text/css" />').attr('href', cssPath));
         // add blobs
-        $("#top-left-blob").attr("src", chrome.runtime.getURL('res/blob-big-2.svg'));
-        $("#bottom-right-blob").attr("src", chrome.runtime.getURL('res/blob-big-1.svg'));
-        $("#small-blob1").attr("src", chrome.runtime.getURL('res/blob-med.svg'));
-        $("#small-blob2").attr("src", chrome.runtime.getURL('res/blob-small.svg'));
+        $('#top-left-blob').attr('src', chrome.runtime.getURL('res/blob-big-2.svg'));
+        $('#bottom-right-blob').attr('src', chrome.runtime.getURL('res/blob-big-1.svg'));
+        $('#small-blob1').attr('src', chrome.runtime.getURL('res/blob-med.svg'));
+        $('#small-blob2').attr('src', chrome.runtime.getURL('res/blob-small.svg'));
     });
 }
 function addFormListener(strippedURL) {
     var _a;
-    const form = document.forms.namedItem("inputForm");
+    const form = document.forms.namedItem('inputForm');
     // add listener for form submit
     (_a = form) === null || _a === void 0 ? void 0 : _a.addEventListener('submit', (event) => {
         // prevent default submit
@@ -92,7 +92,7 @@ function addFormListener(strippedURL) {
         const intentForm = event.target;
         const intent = new FormData(intentForm).get('intent');
         const intentString = intent.toString();
-        const intentDate = new Date;
+        const intentDate = new Date();
         callBackgroundWithIntent(intentString);
         addToStorage(intentString, intentDate, strippedURL);
     });
@@ -116,22 +116,24 @@ function addToStorage(intentString, intentDate, url) {
         }
         // adding new intent and date to intent list
         intentList[intentDate.toJSON()] = {
-            "intent": intentString,
-            "url": url,
+            intent: intentString,
+            url: url,
         };
         // saving intentList to chrome storage
-        chrome.storage.sync.set({ 'intentList': intentList }, () => {
+        chrome.storage.sync.set({ intentList: intentList }, () => {
             console.log('the intent "' + intentString + '" has been added');
         });
     });
 }
 function callBackgroundWithIntent(intent) {
     // open connection to runtime (background.ts)
-    const port = chrome.runtime.connect({ name: "intentStatus" });
+    const port = chrome.runtime.connect({
+        name: 'intentStatus',
+    });
     port.postMessage({ intent: intent, url: window.location.href });
     port.onMessage.addListener((msg) => {
         switch (msg.status) {
-            case "ok":
+            case 'ok':
                 // show success message
                 // optional: transition?
                 chrome.storage.sync.get(null, (storage) => {
@@ -140,18 +142,18 @@ function callBackgroundWithIntent(intent) {
                     location.reload();
                 });
                 break;
-            case "too_short":
-                $('#textbox').effect("shake");
+            case 'too_short':
+                $('#textbox').effect('shake');
                 // display message
-                displayStatus("your response is a little short. be more specific!", 3000, REFLECT_ERR);
-                $("#textbox").val("");
+                displayStatus('your response is a little short. be more specific!', 3000, REFLECT_ERR);
+                $('#textbox').val('');
                 break;
-            case "invalid":
-                $('#textbox').effect("shake");
+            case 'invalid':
+                $('#textbox').effect('shake');
                 // display message
                 displayStatus("that doesn't seem to be productive. try being more specific.", 3000, REFLECT_ERR);
                 // clear input
-                $("#textbox").val("");
+                $('#textbox').val('');
                 break;
         }
         // close connection
