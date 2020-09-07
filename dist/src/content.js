63cd95e8a982f4e6d21de6757c4e8f384b01cc18
jackyzha0 <j.zhao2k19@gmail.com>
2020-09-07 11:56:54 -0700

    refactor to use dynamic blobs

--- dist/src/content.js
+++ dist/src/content.js
@@ -86,12 +86,11 @@ function loadBlockPage(strippedURL) {
         $('#linkToOptions').attr('href', chrome.runtime.getURL('res/pages/options.html'));
         // load css
         const cssPath = chrome.runtime.getURL('res/common.css');
-        $('head').append($('<link rel="stylesheet" type="text/css" />').attr('href', cssPath));
-        // add blobs
-        $('#top-left-blob').attr('src', chrome.runtime.getURL('res/blob-big-2.svg'));
-        $('#bottom-right-blob').attr('src', chrome.runtime.getURL('res/blob-big-1.svg'));
-        $('#small-blob1').attr('src', chrome.runtime.getURL('res/blob-med.svg'));
-        $('#small-blob2').attr('src', chrome.runtime.getURL('res/blob-small.svg'));
+        const cssLink = `<link rel="stylesheet" type="text/css" href="${cssPath}">`;
+        document.head.innerHTML += cssLink;
+        // animate background
+        const anim = new BlobAnimation();
+        anim.animate();
     });
 }
 function addFormListener(strippedURL) {
@@ -173,3 +172,197 @@ function callBackgroundWithIntent(intent) {
         port.disconnect();
     });
 }
+class BlobElement {
+    constructor(maxX, maxY, minR, maxR) {
+        this.fill = "#A6B1CE";
+        this.x = this.originalX = Math.random() * maxX;
+        this.y = this.originalY = Math.random() * maxY;
+        this.r = minR + (Math.random() * (maxR - minR));
+        this.element = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+        // set styling
+        this.element.setAttribute("r", this.r.toString());
+        this.element.setAttribute("style", `fill: ${this.fill};`);
+    }
+    // update element
+    update(mouseX, mouseY, repulsion, attraction) {
+        const dx = this.x - mouseX;
+        const dy = this.y - mouseY;
+        const angle = Math.atan2(dy, dx);
+        const dist = repulsion / Math.sqrt(dx * dx + dy * dy);
+        this.x += Math.cos(angle) * dist;
+        this.y += Math.sin(angle) * dist;
+        this.x += (this.originalX - this.x) * attraction;
+        this.y += (this.originalY - this.y) * attraction;
+        this.element.setAttribute("cx", this.x.toString());
+        this.element.setAttribute("cy", this.y.toString());
+    }
+}
+BlobElement.COLOURS = ["#6474AC", "#8B9AC0", "#A6B1CE"];
+class BlobAnimation {
+    constructor() {
+        this.config = {
+            blur: 14,
+            alphaMult: 30,
+            alphaAdd: -10,
+            numElements: 30,
+            circleMinRadius: 15,
+            circleMaxRadius: 60,
+            attraction: 0.1,
+            repulsion: 1000
+        };
+        this.animate = () => {
+            requestAnimationFrame(this.animate);
+            this.elements.forEach(e => {
+                e.update(this.mouseX, this.mouseY, this.config.repulsion, this.config.attraction);
+            });
+        };
+        // grab dom elements
+        this.svg = document.getElementById("svg");
+        this.colorMatrixF = document.getElementById("colorMatrixF");
+        // bind event listeners
+        const body = document.getElementById("reflect-main");
+        window.addEventListener("resize", this.onResize, false);
+        body.addEventListener("mousemove", (e) => {
+            this.mouseX = e.clientX;
+            this.mouseY = e.clientY;
+        }, false);
+        body.addEventListener("mouseleave", this.resetMouse, false);
+        // create initial svg g elements
+        this.onResize();
+        this.resetMouse();
+        this.initElements();
+        this.colorMatrixF.setAttribute("values", `1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 ${this.config.alphaMult} ${this.config.alphaAdd}`);
+    }
+    initElements() {
+        // create group div with namespace
+        this.elements = [];
+        const group = document.createElementNS("http://www.w3.org/2000/svg", "g");
+        this.svg.appendChild(group);
+        for (let i = 0; i < this.config.numElements; i++) {
+            const e = new BlobElement(this.width, this.height, this.config.circleMinRadius, this.config.circleMaxRadius);
+            e.update(this.mouseX, this.mouseY, this.config.repulsion, this.config.attraction);
+            group.appendChild(e.element);
+            this.elements.push(e);
+        }
+    }
+    // set mouse cords back to bottom centre screen
+    resetMouse() {
+        this.mouseX = this.centerX;
+        this.mouseY = 5 * this.centerY;
+    }
+    // recompute width, height, and centre
+    onResize() {
+        this.width = window.innerWidth;
+        this.height = window.innerHeight;
+        this.centerX = this.width / 2;
+        this.centerY = this.height / 2;
+    }
+}
+// function anim(): void {
+//     let width = 0,
+//         height = 0,
+//         cx = 0,
+//         cy = 0;
+//     let svg, group, colorMatrixF;
+//     let conf = {
+//         blur: 14,
+//         redMult: 1,
+//         greenMult: 1,
+//         blueMult: 1,
+//         alphaMult: 30,
+//         alphaAdd: -10,
+//         randomColors: false,
+//         numElements: 30,
+//         circleMinRadius: 15,
+//         circleMaxRadius: 60,
+//         attraction: 0.1,
+//         repulsion: 1000
+//     };
+//     let elements = [];
+//     let mouseX = 0,
+//         mouseY = 0;
+//     function init() {
+//         svg = document.getElementById("svg");
+//         const body = document.getElementById("reflect-main");
+//         colorMatrixF = document.getElementById("colorMatrixF");
+//         onResize();
+//         initElements();
+//         updateColorMatrixF();
+//         resetMouse();
+//         window.addEventListener("resize", onResize, false);
+//         body.addEventListener("mousemove", onMouseMove, false);
+//         body.addEventListener("mouseleave", resetMouse, false);
+//         animate();
+//     }
+//     function initElements() {
+//         elements = [];
+//         if (group) {
+//             svg.removeChild(group);
+//         }
+//         group = document.createElementNS("http://www.w3.org/2000/svg", "g");
+//         svg.appendChild(group);
+//         let fill = REFLECT_INFO;
+//         let x, y, r;
+//         for (let i = 0; i < conf.numElements; i++) {
+//             x = rnd(width);
+//             y = rnd(height);
+//             r = conf.circleMinRadius + rnd(conf.circleMaxRadius - conf.circleMinRadius);
+//             let e: any = { x, y, ox: x, oy: y, r, fill };
+//             e.elt = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+//             updateElement(e);
+//             group.appendChild(e.elt);
+//             elements.push(e);
+//         }
+//     }
+//     function resetMouse() {
+//         mouseX = cx;
+//         mouseY = 5 * cy;
+//     }
+//     function animate() {
+//         requestAnimationFrame(animate);
+//         let dx, dy, dist, angle, x, y;
+//         elements.forEach((e, i) => {
+//             dx = e.x - mouseX;
+//             dy = e.y - mouseY;
+//             angle = Math.atan2(dy, dx);
+//             dist = conf.repulsion / Math.sqrt(dx * dx + dy * dy);
+//             e.x += Math.cos(angle) * dist;
+//             e.y += Math.sin(angle) * dist;
+//             e.x += (e.ox - e.x) * conf.attraction;
+//             e.y += (e.oy - e.y) * conf.attraction;
+//             updateElementPos(e);
+//         });
+//     }
+//     function updateElement(e) {
+//         updateElementPos(e);
+//         e.elt.setAttribute("r", e.r);
+//         e.elt.setAttribute("style", estyle(e));
+//     }
+//     function updateElementPos(e) {
+//         e.elt.setAttribute("cx", e.x);
+//         e.elt.setAttribute("cy", e.y);
+//     }
+//     function estyle(e) {
+//         return `fill: ${e.fill};`;
+//     }
+//     function updateColorMatrixF() {
+//         colorMatrixF.setAttribute("values", colorMatrixValues());
+//     }
+//     function onResize() {
+//         width = window.innerWidth;
+//         height = window.innerHeight;
+//         cx = width / 2;
+//         cy = height / 2;
+//     }
+//     function onMouseMove(e) {
+//         mouseX = e.clientX;
+//         mouseY = e.clientY;
+//     }
+//     function colorMatrixValues() {
+//         return `${conf.redMult} 0 0 0 0 0 ${conf.greenMult} 0 0 0 0 0 ${conf.blueMult} 0 0 0 0 0 ${conf.alphaMult} ${conf.alphaAdd}`;
+//     }
+//     function rnd(max, negative = false) {
+//         return negative ? Math.random() * 2 * max - max : Math.random() * max;
+//     }
+//     init();
+// }
--- src/content.ts
+++ src/content.ts
@@ -107,13 +107,13 @@ function loadBlockPage(strippedURL: string): void {
 
         // load css
         const cssPath: string = chrome.runtime.getURL('res/common.css')
-        $('head').append($('<link rel="stylesheet" type="text/css" />').attr('href', cssPath))
+        const cssLink: string = `<link rel="stylesheet" type="text/css" href="${cssPath}">`
 
-        // add blobs
-        $('#top-left-blob').attr('src', chrome.runtime.getURL('res/blob-big-2.svg'))
-        $('#bottom-right-blob').attr('src', chrome.runtime.getURL('res/blob-big-1.svg'))
-        $('#small-blob1').attr('src', chrome.runtime.getURL('res/blob-med.svg'))
-        $('#small-blob2').attr('src', chrome.runtime.getURL('res/blob-small.svg'))
+        document.head.innerHTML += cssLink
+
+        // animate background
+        const anim = new BlobAnimation()
+        anim.animate()
     })
 }
 
@@ -221,4 +221,149 @@ function callBackgroundWithIntent(intent: string): void {
         // close connection
         port.disconnect()
     })
+}
+
+interface BlobConfig {
+    blur: number
+    alphaMult: number
+    alphaAdd: number
+    numElements: number
+    circleMinRadius: number
+    circleMaxRadius: number
+    attraction: number
+    repulsion: number
+}
+
+class BlobElement {
+    fill: string = '#A6B1CE'
+    x: number
+    y: number
+    r: number
+    originalX: number
+    originalY: number
+    element: SVGCircleElement
+
+    static COLOURS: string[] = ['#6474AC', '#8B9AC0', '#A6B1CE']
+
+    constructor(maxX: number, maxY: number, minR: number, maxR: number) {
+        this.x = this.originalX = Math.random() * maxX
+        this.y = this.originalY = Math.random() * maxY
+        this.r = minR + Math.random() * (maxR - minR)
+        this.element = document.createElementNS('http://www.w3.org/2000/svg', 'circle')
+
+        // set styling
+        this.element.setAttribute('r', this.r.toString())
+        this.element.setAttribute('style', `fill: ${this.fill};`)
+    }
+
+    // update element
+    update(mouseX: number, mouseY: number, repulsion: number, attraction: number): void {
+        const dx = this.x - mouseX
+        const dy = this.y - mouseY
+        const angle = Math.atan2(dy, dx)
+        const dist = repulsion / Math.sqrt(dx * dx + dy * dy)
+        this.x += Math.cos(angle) * dist
+        this.y += Math.sin(angle) * dist
+        this.x += (this.originalX - this.x) * attraction
+        this.y += (this.originalY - this.y) * attraction
+
+        this.element.setAttribute('cx', this.x.toString())
+        this.element.setAttribute('cy', this.y.toString())
+    }
+}
+
+class BlobAnimation {
+    width: number
+    height: number
+    centerX: number
+    centerY: number
+
+    // dom refs
+    svg: HTMLElement
+    colorMatrixF: HTMLElement
+    elements: BlobElement[]
+
+    // mouse
+    mouseX: number
+    mouseY: number
+
+    config: BlobConfig = {
+        blur: 14,
+        alphaMult: 30,
+        alphaAdd: -10,
+        numElements: 30,
+        circleMinRadius: 15,
+        circleMaxRadius: 60,
+        attraction: 0.1,
+        repulsion: 1000,
+    }
+
+    constructor() {
+        // grab dom elements
+        this.svg = document.getElementById('svg')
+        this.colorMatrixF = document.getElementById('colorMatrixF')
+
+        // bind event listeners
+        const body = document.getElementById('reflect-main')
+        window.addEventListener('resize', this.onResize, false)
+        body.addEventListener(
+            'mousemove',
+            (e) => {
+                this.mouseX = e.clientX
+                this.mouseY = e.clientY
+            },
+            false
+        )
+        body.addEventListener('mouseleave', this.resetMouse, false)
+
+        // create initial svg g elements
+        this.onResize()
+        this.resetMouse()
+        this.initElements()
+        this.colorMatrixF.setAttribute(
+            'values',
+            `1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 ${this.config.alphaMult} ${this.config.alphaAdd}`
+        )
+    }
+
+    private initElements(): void {
+        // create group div with namespace
+        this.elements = []
+        const group: SVGElement = document.createElementNS('http://www.w3.org/2000/svg', 'g')
+        this.svg.appendChild(group)
+
+        for (let i = 0; i < this.config.numElements; i++) {
+            const e: BlobElement = new BlobElement(
+                this.width,
+                this.height,
+                this.config.circleMinRadius,
+                this.config.circleMaxRadius
+            )
+
+            e.update(this.mouseX, this.mouseY, this.config.repulsion, this.config.attraction)
+            group.appendChild(e.element)
+            this.elements.push(e)
+        }
+    }
+
+    // set mouse cords back to bottom centre screen
+    private resetMouse(): void {
+        this.mouseX = this.centerX
+        this.mouseY = 5 * this.centerY
+    }
+
+    // recompute width, height, and centre
+    private onResize(): void {
+        this.width = window.innerWidth
+        this.height = window.innerHeight
+        this.centerX = this.width / 2
+        this.centerY = this.height / 2
+    }
+
+    animate = () => {
+        requestAnimationFrame(this.animate)
+        this.elements.forEach((e) => {
+            e.update(this.mouseX, this.mouseY, this.config.repulsion, this.config.attraction)
+        })
+    }
 }
