d7dfb96f75ff181b8712d13e66b9060c4f9a6944
jackyzha0 <j.zhao2k19@gmail.com>
2020-09-08 09:36:58 -0700

    fix ff distro and bump versions

--- ff-dist/src/content.js
+++ ff-dist/src/content.js
@@ -1,51 +1,64 @@
-const REFLECT_INFO = "#576ca8";
-const REFLECT_ERR = "#ff4a47";
-chrome.storage.sync.get(null, (storage) => {
-    // check to see if reflect is enabled
-    if (storage.isEnabled) {
-        // check for is blocked
-        const strippedURL = getStrippedUrl();
-        storage.blockedSites.forEach((site) => {
-            // is blocked
-            if (strippedURL.includes(site)) {
-                iterWhitelist();
+const REFLECT_INFO = '#576ca8';
+const REFLECT_ERR = '#ff4a47';
+const WHITELISTED_WRAPPERS = ['facebook.com/flx', 'l.facebook.com'];
+checkIfBlocked();
+// re-check page everytime this page gets focus again
+window.addEventListener('focus', checkIfBlocked);
+function checkIfBlocked() {
+    // check if already blocked
+    if (!!document.getElementById('reflectMain') === false) {
+        chrome.storage.sync.get(null, (storage) => {
+            // check to see if reflect is enabled
+            if (storage.isEnabled) {
+                // check for is blocked
+                const strippedURL = getStrippedUrl();
+                storage.blockedSites.forEach((site) => {
+                    // is blocked and not a whitelisted wrapper
+                    if (strippedURL.includes(site) && !isWhitelistedWrapper()) {
+                        iterWhitelist();
+                    }
+                });
             }
         });
     }
-});
+}
 function displayStatus(message, duration = 3000, colour = REFLECT_INFO) {
     // set content
-    $("#statusContent").css("color", colour);
-    $("#statusContent").text(message);
+    $('#statusContent').css('color', colour);
+    $('#statusContent').text(message);
     // show, wait, then hide
-    $("#statusContent").show().delay(duration).fadeOut();
+    $('#statusContent').show().delay(duration).fadeOut();
+}
+function isWhitelistedWrapper() {
+    // check if any wrapper urls are present in current url
+    return WHITELISTED_WRAPPERS.some((wrapper) => window.location.href.includes(wrapper));
 }
 function getStrippedUrl() {
     // match url
     const activeURL = window.location.href.match(/^[\w]+:\/{2}([\w\.:-]+)/);
     if (activeURL != null) {
-        const strippedURL = activeURL[1].replace("www.", "");
+        const strippedURL = activeURL[1].replace('www.', '');
         return strippedURL;
     }
     // no url?
-    return "";
+    return '';
 }
 function iterWhitelist() {
     // iterate whitelisted sites
     chrome.storage.sync.get(null, (storage) => {
         const strippedURL = getStrippedUrl();
         // activeURL exists
-        if (strippedURL != "") {
+        if (strippedURL != '') {
             console.log(strippedURL);
             // if url in whitelist
             const m = storage.whitelistedSites;
             if (m.hasOwnProperty(strippedURL)) {
-                console.log("whitelisted");
+                console.log('whitelisted');
                 // check if expired
                 const parsedDate = new Date(m[strippedURL]);
                 const currentDate = new Date();
                 if (currentDate >= parsedDate) {
-                    console.log("expired");
+                    console.log('expired');
                     loadBlockPage(strippedURL);
                 }
                 else {
@@ -55,7 +68,7 @@ function iterWhitelist() {
                 }
             }
             else {
-                console.log("blocked");
+                console.log('blocked');
                 loadBlockPage(strippedURL);
             }
         }
@@ -64,26 +77,25 @@ function iterWhitelist() {
 }
 function loadBlockPage(strippedURL) {
     // get prompt page content
-    $.get(chrome.runtime.getURL("res/pages/prompt.html"), (page) => {
+    $.get(chrome.runtime.getURL('res/pages/prompt.html'), (page) => {
         // stop current page and replace with our blocker page
         window.stop();
         $('html').html(page);
         addFormListener(strippedURL);
         // inject show options page
-        $("#linkToOptions").attr("href", chrome.runtime.getURL('res/pages/options.html'));
+        $('#linkToOptions').attr('href', chrome.runtime.getURL('res/pages/options.html'));
         // load css
         const cssPath = chrome.runtime.getURL('res/common.css');
-        $("head").append($('<link rel="stylesheet" type="text/css" />').attr('href', cssPath));
-        // add blobs
-        $("#top-left-blob").attr("src", chrome.runtime.getURL('res/blob-big-2.svg'));
-        $("#bottom-right-blob").attr("src", chrome.runtime.getURL('res/blob-big-1.svg'));
-        $("#small-blob1").attr("src", chrome.runtime.getURL('res/blob-med.svg'));
-        $("#small-blob2").attr("src", chrome.runtime.getURL('res/blob-small.svg'));
+        const cssLink = `<link rel="stylesheet" type="text/css" href="${cssPath}">`;
+        document.head.innerHTML += cssLink;
+        // animate background
+        const anim = new BlobAnimation();
+        anim.animate();
     });
 }
 function addFormListener(strippedURL) {
     var _a;
-    const form = document.forms.namedItem("inputForm");
+    const form = document.forms.namedItem('inputForm');
     // add listener for form submit
     (_a = form) === null || _a === void 0 ? void 0 : _a.addEventListener('submit', (event) => {
         // prevent default submit
@@ -92,7 +104,7 @@ function addFormListener(strippedURL) {
         const intentForm = event.target;
         const intent = new FormData(intentForm).get('intent');
         const intentString = intent.toString();
-        const intentDate = new Date;
+        const intentDate = new Date();
         callBackgroundWithIntent(intentString);
         addToStorage(intentString, intentDate, strippedURL);
     });
@@ -116,22 +128,24 @@ function addToStorage(intentString, intentDate, url) {
         }
         // adding new intent and date to intent list
         intentList[intentDate.toJSON()] = {
-            "intent": intentString,
-            "url": url,
+            intent: intentString,
+            url: url,
         };
         // saving intentList to chrome storage
-        chrome.storage.sync.set({ 'intentList': intentList }, () => {
+        chrome.storage.sync.set({ intentList: intentList }, () => {
             console.log('the intent "' + intentString + '" has been added');
         });
     });
 }
 function callBackgroundWithIntent(intent) {
     // open connection to runtime (background.ts)
-    const port = chrome.runtime.connect({ name: "intentStatus" });
+    const port = chrome.runtime.connect({
+        name: 'intentStatus',
+    });
     port.postMessage({ intent: intent, url: window.location.href });
     port.onMessage.addListener((msg) => {
         switch (msg.status) {
-            case "ok":
+            case 'ok':
                 // show success message
                 // optional: transition?
                 chrome.storage.sync.get(null, (storage) => {
@@ -140,21 +154,124 @@ function callBackgroundWithIntent(intent) {
                     location.reload();
                 });
                 break;
-            case "too_short":
-                $('#textbox').effect("shake");
+            case 'too_short':
+                $('#inputFields').effect('shake', { times: 3, distance: 5 });
                 // display message
-                displayStatus("your response is a little short. be more specific!", 3000, REFLECT_ERR);
-                $("#textbox").val("");
+                displayStatus('your response is a little short. be more specific!', 3000, REFLECT_ERR);
+                $('#textbox').val('');
                 break;
-            case "invalid":
-                $('#textbox').effect("shake");
+            case 'invalid':
+                $('#inputFields').effect('shake', { times: 3, distance: 5 });
                 // display message
                 displayStatus("that doesn't seem to be productive. try being more specific.", 3000, REFLECT_ERR);
                 // clear input
-                $("#textbox").val("");
+                $('#textbox').val('');
                 break;
         }
         // close connection
         port.disconnect();
     });
+}
+class BlobElement {
+    constructor(x, y, r) {
+        this.fill = '#A6B1CE';
+        this.x = this.originalX = x;
+        this.y = this.originalY = y;
+        this.r = r || 10;
+        this.element = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+        // set styling
+        this.element.setAttribute('r', this.r.toString());
+        this.element.setAttribute('style', `fill: ${this.fill};`);
+    }
+    // update element
+    update(mouseX, mouseY, repulsion, attraction) {
+        const dx = this.x - mouseX;
+        const dy = this.y - mouseY;
+        const angle = Math.atan2(dy, dx);
+        const dist = repulsion / Math.sqrt(dx * dx + dy * dy);
+        this.x += Math.cos(angle) * dist;
+        this.y += Math.sin(angle) * dist;
+        this.x += (this.originalX - this.x) * attraction;
+        this.y += (this.originalY - this.y) * attraction;
+        this.element.setAttribute('cx', this.x.toString());
+        this.element.setAttribute('cy', this.y.toString());
+    }
+}
+class BlobAnimation {
+    constructor() {
+        this.config = {
+            blur: 8,
+            alphaMult: 30,
+            alphaAdd: -10,
+            numSeeds: 8,
+            childrenPerSeed: 3,
+            childrenDistanceRange: 100,
+            circleMinRadius: 15,
+            circleMaxRadius: 75,
+            attraction: 0.1,
+            repulsion: 1000,
+        };
+        this.animate = () => {
+            requestAnimationFrame(this.animate);
+            this.elements.forEach((e) => {
+                e.update(this.mouseX, this.mouseY, this.config.repulsion, this.config.attraction);
+            });
+        };
+        // grab dom elements
+        this.svg = document.getElementById('svg');
+        this.colorMatrixF = document.getElementById('colorMatrixF');
+        // bind event listeners
+        const body = document.getElementById('reflect-main');
+        window.addEventListener('resize', this.onResize, false);
+        body.addEventListener('mousemove', (e) => {
+            this.mouseX = e.clientX;
+            this.mouseY = e.clientY;
+        }, false);
+        body.addEventListener('mouseleave', this.resetMouse, false);
+        // create initial svg g elements
+        this.onResize();
+        this.resetMouse();
+        this.initElements();
+        this.colorMatrixF.setAttribute('values', `1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 ${this.config.alphaMult} ${this.config.alphaAdd}`);
+    }
+    random(min, max) {
+        return min + Math.random() * (max - min);
+    }
+    randomRange(targ, range) {
+        return targ + (Math.random() * 2 - 1) * range;
+    }
+    initElements() {
+        // create group div with namespace
+        this.elements = [];
+        const group = document.createElementNS('http://www.w3.org/2000/svg', 'g');
+        this.svg.appendChild(group);
+        // create seeds
+        for (let i = 0; i < this.config.numSeeds; i++) {
+            const e = new BlobElement(this.randomRange(this.centerX, this.width * 0.4), this.randomRange(this.centerY, this.height * 0.4), this.random(this.config.circleMinRadius, this.config.circleMaxRadius));
+            e.update(this.mouseX, this.mouseY, this.config.repulsion, this.config.attraction);
+            group.appendChild(e.element);
+            this.elements.push(e);
+        }
+        // add children to seeds
+        this.elements.forEach((e) => {
+            for (let j = 0; j < this.config.childrenPerSeed; j++) {
+                const child = new BlobElement(this.randomRange(e.x, this.config.childrenDistanceRange), this.randomRange(e.y, this.config.childrenDistanceRange), this.random(this.config.circleMinRadius, this.config.circleMaxRadius));
+                child.update(this.mouseX, this.mouseY, this.config.repulsion, this.config.attraction);
+                group.appendChild(child.element);
+                this.elements.push(child);
+            }
+        });
+    }
+    // set mouse cords back to bottom centre screen
+    resetMouse() {
+        this.mouseX = this.centerX;
+        this.mouseY = 5 * this.centerY;
+    }
+    // recompute width, height, and centre
+    onResize() {
+        this.width = window.innerWidth;
+        this.height = window.innerHeight;
+        this.centerX = this.width / 2;
+        this.centerY = this.height / 2;
+    }
 }
--- ff-dist/src/options.js
+++ ff-dist/src/options.js
@@ -1,6 +1,7 @@
 const ENTER_KEY_CODE = 13;
 // On page load, draw table and add button listener
-document.addEventListener('DOMContentLoaded', function renderFilterListTable() {
+document.addEventListener('DOMContentLoaded', () => {
+    // setup button listeners and draw tables
     drawFilterListTable();
     drawIntentListTable();
     setAddButtonListener();
@@ -11,14 +12,53 @@ document.addEventListener('DOMContentLoaded', function renderFilterListTable() {
 function setupOptionsListener() {
     document.getElementById('save').addEventListener('click', saveCurrentOptions);
 }
+// taken from https://www.w3schools.com/howto/howto_js_sort_table.asp
+function sortTable() {
+    var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+    table = document.getElementById('intentList');
+    switching = true;
+    dir = 'asc';
+    while (switching) {
+        switching = false;
+        rows = table.rows;
+        for (i = 1; i < rows.length - 1; i++) {
+            shouldSwitch = false;
+            x = rows[i].getElementsByTagName('td')[0].childNodes[0];
+            y = rows[i + 1].getElementsByTagName('td')[0].childNodes[0];
+            if (dir == 'asc') {
+                if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                    shouldSwitch = true;
+                    break;
+                }
+            }
+            else if (dir == 'desc') {
+                if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+                    shouldSwitch = true;
+                    break;
+                }
+            }
+        }
+        if (shouldSwitch) {
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+            switching = true;
+            switchcount++;
+        }
+        else {
+            if (switchcount == 0 && dir == 'asc') {
+                dir = 'desc';
+                switching = true;
+            }
+        }
+    }
+}
 function saveCurrentOptions() {
     // get all form values
     const whitelistTimeElement = document.getElementById('whitelistTime');
     const whitelistTime = whitelistTimeElement.value;
     const numIntentEntriesElement = document.getElementById('numIntentEntries');
     const numIntentEntries = numIntentEntriesElement.value;
-    chrome.storage.sync.set({ 'numIntentEntries': numIntentEntries }, () => {
-        chrome.storage.sync.set({ 'whitelistTime': whitelistTime }, () => {
+    chrome.storage.sync.set({ numIntentEntries: numIntentEntries }, () => {
+        chrome.storage.sync.set({ whitelistTime: whitelistTime }, () => {
             // Update status to let user know options were saved.
             const status = document.getElementById('statusContent');
             status.textContent = 'options saved.';
@@ -30,51 +70,57 @@ function saveCurrentOptions() {
 }
 function restoreSavedOptions() {
     chrome.storage.sync.get(null, (storage) => {
+        ;
         document.getElementById('whitelistTime').value = storage.whitelistTime;
-        document.getElementById('numIntentEntries').value = storage.numIntentEntries;
+        document.getElementById('numIntentEntries').value =
+            storage.numIntentEntries;
     });
 }
 function updateButtonListeners() {
     // get all buttons
-    const buttons = document.getElementsByTagName("button");
+    const buttons = document.getElementsByTagName('button');
     for (const button of buttons) {
-        button.addEventListener("click", () => {
+        button.addEventListener('click', () => {
             var _a;
             // get button ID
             const id = parseInt(button.id[0]);
             // get url
-            const url = (_a = document.getElementById(button.id[0] + "site")) === null || _a === void 0 ? void 0 : _a.innerHTML;
+            const url = (_a = document.getElementById(button.id[0] + 'site')) === null || _a === void 0 ? void 0 : _a.innerHTML;
             // get blockedSites
             chrome.storage.sync.get(null, (storage) => {
                 const blockedSites = storage.blockedSites;
                 // remove by ID
                 blockedSites.splice(id, 1);
                 // sync with chrome storage
-                chrome.storage.sync.set({ 'blockedSites': blockedSites }, () => {
+                chrome.storage.sync.set({ blockedSites: blockedSites }, () => {
                     console.log(`removed ${url} from blocked list`);
                     drawFilterListTable();
                 });
             });
         });
     }
-    ;
 }
-;
 function generateWebsiteDiv(id, site) {
-    return "<tr>" +
+    return ('<tr>' +
         `<td style="width: 95%"><p class="urlDisplay" id=${id}>${site}</p></td>` +
         `<td style="width: 5%"><button id=${id}>&times;</button></td>` +
-        "</tr>";
+        '</tr>');
 }
 function generateIntentDiv(id, intent, date, url) {
     // reformatting date to only include month, date, and 12 hour time
-    const formattedDate = date.toLocaleDateString('default', { month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', hour12: true });
+    const formattedDate = date.toLocaleDateString('default', {
+        month: 'long',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+        hour12: true,
+    });
     // creating display table for intents and dates
-    return "<tr>" +
+    return ('<tr>' +
         `<td style="width: 40%"><p class="intentDisplay" id=${id}>${url}</p></td>` +
         `<td style="width: 40%"><p class="intentDisplay" id=${id}>${intent}</p></td>` +
         `<td style="width: 20%"><p class="intentDisplay" id=${id}>${formattedDate}</p></td>` +
-        "</tr>";
+        '</tr>');
 }
 function drawFilterListTable() {
     // accessing chrome storage for blocked sites
@@ -90,7 +136,7 @@ function drawFilterListTable() {
             cur_id++;
         });
         // generates new line in table for new intent
-        table += "</table>";
+        table += '</table>';
         // adds table to html
         const filterList = document.getElementById('filterList');
         if (filterList != null) {
@@ -100,19 +146,18 @@ function drawFilterListTable() {
         updateButtonListeners();
     });
 }
-;
 function drawIntentListTable() {
     // accessing chrome storage for intents
     chrome.storage.sync.get(null, (storage) => {
         // fetch intent list
         const intentList = storage.intentList;
         // generate table element
-        let table = '<table class="hover shadow styled">' +
-            "<tr>" +
-            '<th style="width: 40%">url</th>' +
+        let table = '<table id="intentList" class="hover shadow styled">' +
+            '<tr>' +
+            '<th id="urlHeader" style="width: 40%">url</th>' +
             '<th style="width: 40%">intent</th>' +
             '<th style="width: 20%">date</th>' +
-            "</tr>";
+            '</tr>';
         let cur_id = 0;
         // iter dates in intentList
         for (const rawDate in intentList) {
@@ -128,21 +173,22 @@ function drawIntentListTable() {
             }
         }
         // generates new line in table for new intent
-        table += "</table>";
+        table += '</table>';
         // insert table into html
         const previousIntents = document.getElementById('previousIntents');
         if (previousIntents != null) {
             previousIntents.innerHTML = table;
         }
+        // setup table sort events
+        document.getElementById('urlHeader').addEventListener('click', sortTable);
     });
 }
-;
 // sets event listeners for add new url operations
 function setAddButtonListener() {
     const urlInputElement = document.getElementById('urlInput');
     // add key listener to submit new url on <ENTER> pressed
-    urlInputElement.addEventListener("keypress", (event) => {
-        if (event.keyCode == ENTER_KEY_CODE) {
+    urlInputElement.addEventListener('keypress', (event) => {
+        if (event.keyCode === ENTER_KEY_CODE) {
             addUrlToFilterList();
         }
     });
@@ -152,26 +198,24 @@ function setAddButtonListener() {
         addUrlToFilterList();
     });
 }
-;
 function addUrlToFilterList() {
     // get urlInput
     const urlInput = document.getElementById('urlInput');
     // see if value is non-empty
-    if (urlInput.value != "") {
+    if (urlInput.value !== '') {
         chrome.storage.sync.get(null, (storage) => {
             // get current blocked sites
             const blockedSites = storage.blockedSites;
             // add to blocked sites
             blockedSites.push(urlInput.value);
             // sync changes with chrome storage
-            chrome.storage.sync.set({ 'blockedSites': blockedSites }, () => {
+            chrome.storage.sync.set({ blockedSites: blockedSites }, () => {
                 console.log(`added ${urlInput} from blocked list`);
                 // clear input
-                urlInput.value = "";
+                urlInput.value = '';
                 // redraw filterList
                 drawFilterListTable();
             });
         });
     }
 }
-;
