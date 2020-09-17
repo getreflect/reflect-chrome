12d8acf09839594cf2ab588fb4f9ffc81c7cd7d8
jackyzha0 <j.zhao2k19@gmail.com>
2020-09-17 00:10:30 -0700

    fix blobs being oval insted of round and fix font size bug on youtube

--- src/background.ts
+++ src/background.ts
@@ -319,3 +319,44 @@ function reloadActive(): void {
         chrome.tabs.reload(tabs[0].id)
     })
 }
+
+// browser actions
+chrome.contextMenus.create({
+    id: 'baAddToFilterList',
+    title: 'Block this:',
+    contexts: ['browser_action'],
+})
+
+chrome.contextMenus.create({
+    parentId: 'baAddToFilterList',
+    id: 'baAddSiteToFilterList',
+    title: 'Page',
+    contexts: ['browser_action'],
+})
+
+chrome.contextMenus.create({
+    parentId: 'baAddToFilterList',
+    id: 'baAddDomainToFilterList',
+    title: 'Domain',
+    contexts: ['browser_action'],
+})
+
+chrome.contextMenus.create({
+    id: 'pgAddToFilterList',
+    title: 'Block this:',
+    contexts: ['page'],
+})
+
+chrome.contextMenus.create({
+    parentId: 'pgAddToFilterList',
+    id: 'pgAddSiteToFilterList',
+    title: 'Page',
+    contexts: ['page'],
+})
+
+chrome.contextMenus.create({
+    parentId: 'pgAddToFilterList',
+    id: 'pgAddDomainToFilterList',
+    title: 'Domain',
+    contexts: ['page'],
+})
