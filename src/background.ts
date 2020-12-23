88705f17dabed61121b918446bd9f257bafd2e3f
jackyzha0 <j.zhao2k19@gmail.com>
2020-12-22 16:51:01 -0800

    refactor background handlers into separate funcs

--- src/background.ts
+++ src/background.ts
@@ -77,6 +77,37 @@ chrome.runtime.onStartup.addListener(() => {
   })
 })
 
+function turnFilteringOff(): void {
+  setStorage({ isEnabled: false }).then(() => {
+    // stop checking for badge updates
+    cleanupBadge()
+
+    chrome.browserAction.setIcon({ path: 'res/off.png' }, () => {
+      console.log('Filtering disabled')
+    })
+    reloadActive()
+  })
+}
+
+function turnFilteringOn(): void {
+  setStorage({ isEnabled: false }).then(() => {
+    // start badge update counter
+    setBadgeUpdate()
+
+    chrome.browserAction.setIcon({ path: 'res/on.png' }, () => {
+      console.log('Filtering enabled.')
+    })
+    reloadActive()
+  })
+}
+
+// reloads tab that is currently in focus
+function reloadActive(): void {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.tabs.reload(tabs[0].id)
+  })
+}
+
 // Catch menu clicks (page context and browser action context)
 chrome.contextMenus.onClicked.addListener((info, tab) => {
   switch (info.menuItemId) {
@@ -107,107 +138,85 @@ setupContextMenus()
 // Load ML model
 const model: nn.IntentClassifier = new nn.IntentClassifier('acc85.95')
 
-// Listen for new runtime connections
+// Listen for new signals from non-background scripts
 chrome.runtime.onConnect.addListener((port) => {
   // check comm channel
   switch (port.name) {
     // listens for messages from content scripts
     case 'intentStatus': {
-      port.onMessage.addListener(async (msg) => {
-        // extract intent and url from message
-        const intent: string = msg.intent
-
-        // get whitelist period
-        getStorage().then(async (storage) => {
-          const WHITELIST_PERIOD: number = storage.whitelistTime
-          const words: string[] = intent.split(' ')
-
-          if (words.length <= 3) {
-            // if too short, let content script know and early return
-            port.postMessage({ status: 'too_short' })
-            return
-          }
-
-          // send to nlp model for prediction
-          const valid: boolean = await model.predict(intent)
-          if (!valid) {
-            // if invalid, let content script know and early return
-            port.postMessage({ status: 'invalid' })
-            console.log('Failed. Remaining on page.')
-            return
-          }
-
-          // add whitelist period for site
-          chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-            const urls: string[] = tabs.map((x) => x.url)
-            const domain: string = cleanDomain(urls)
-            addToWhitelist(domain, WHITELIST_PERIOD)
-          })
-
-          // send status to tab
-          port.postMessage({ status: 'ok' })
-          console.log(`Success! Redirecting`)
-        })
-      })
+      port.onMessage.addListener((msg) => intentHandler(port, msg))
     }
 
     // listens for messages from popup
     case 'toggleState': {
-      port.onMessage.addListener((msg) => {
-        const on: boolean = msg.state
-        if (on) {
-          turnFilteringOn()
-        } else if (on === false) {
-          turnFilteringOff()
-        }
-      })
+      port.onMessage.addListener((msg) => toggleStateHandler(port, msg))
     }
 
     // listens for block from popup
     case 'blockFromPopup': {
-      port.onMessage.addListener((msg) => {
-        const url: string = msg.siteURL
-        const unblock: boolean = msg.unblock
-        if (url !== undefined && url !== '' && unblock !== undefined) {
-          if (unblock) {
-            removeFromBlocked(url)
-          } else if (!unblock) {
-            addToBlocked(url)
-          }
-          reloadActive()
-        }
-      })
+      port.onMessage.addListener((msg) => blockFromPopupHandler(port, msg))
     }
   }
 })
 
-function turnFilteringOff(): void {
-  setStorage({ isEnabled: false }).then(() => {
-    // stop checking for badge updates
-    cleanupBadge()
+// handle content script intent submission
+async function intentHandler(port: chrome.runtime.Port, msg) {
+  // extract intent and url from message
+  const intent: string = msg.intent
 
-    chrome.browserAction.setIcon({ path: 'res/off.png' }, () => {
-      console.log('Filtering disabled')
+  // get whitelist period
+  getStorage().then(async (storage) => {
+    const WHITELIST_PERIOD: number = storage.whitelistTime
+    const words: string[] = intent.split(' ')
+
+    if (words.length <= 3) {
+      // if too short, let content script know and early return
+      port.postMessage({ status: 'too_short' })
+      return
+    }
+
+    // send to nlp model for prediction
+    const valid: boolean = await model.predict(intent)
+    if (!valid) {
+      // if invalid, let content script know and early return
+      port.postMessage({ status: 'invalid' })
+      console.log('Failed. Remaining on page.')
+      return
+    }
+
+    // add whitelist period for site
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      const urls: string[] = tabs.map((x) => x.url)
+      const domain: string = cleanDomain(urls)
+      addToWhitelist(domain, WHITELIST_PERIOD)
     })
-    reloadActive()
+
+    // send status to tab
+    port.postMessage({ status: 'ok' })
+    console.log(`Success! Redirecting`)
   })
 }
 
-function turnFilteringOn(): void {
-  setStorage({ isEnabled: false }).then(() => {
-    // start badge update counter
-    setBadgeUpdate()
-
-    chrome.browserAction.setIcon({ path: 'res/on.png' }, () => {
-      console.log('Filtering enabled.')
-    })
-    reloadActive()
-  })
+// handle user toggling extension on/off
+function toggleStateHandler(port: chrome.runtime.Port, msg) {
+  const on: boolean = msg.state
+  if (on) {
+    turnFilteringOn()
+  } else if (on === false) {
+    turnFilteringOff()
+  }
 }
 
-// reloads tab that is currently in focus
-function reloadActive(): void {
-  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    chrome.tabs.reload(tabs[0].id)
-  })
+// handle user blocking current site from popup
+function blockFromPopupHandler(port: chrome.runtime.Port, msg) {
+  const url: string = msg.siteURL
+  const unblock: boolean = msg.unblock
+  if (url !== undefined && url !== '' && unblock !== undefined) {
+    if (unblock) {
+      removeFromBlocked(url)
+    } else if (!unblock) {
+      addToBlocked(url)
+    }
+    reloadActive()
+  }
 }
